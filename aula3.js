3378124dedeb457fe758cf60aff89bf433bddafe
Edir Dumaszak <ecaramba@gmail.com>
2021-09-24 19:26:42 -0300

    exercicios

---
+++ aula3.js
@@ -0,0 +1,17 @@
+// logica
+
+/*
+
+Logica com javascript
+
+ 
+1 - Colocar a primeira letra da palavra em maiuscula
+Ex. curitiba -> Curitiba
+CURITIBA -> Curitiba
+CuRITiba -> Curitiba
+
+2 - Você trabalha num posto de combustivel porém a bomba está quebrada e não mostra o total a ser pago. Crie uma função que informe o total a pagar com o valor por litro e quantidade informadas pelo frentista.
+
+3 - Faça uma pesquisa em um texto e informe se foi encontrado ou não
+
+*/
