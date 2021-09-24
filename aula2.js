629bc9807400c69e1d94ffddde0328a0b0ca8c83
Edir Dumaszak <ecaramba@gmail.com>
2021-09-24 19:09:09 -0300

    aula2

---
+++ aula2.js
@@ -0,0 +1,46 @@
+// aula2
+// Javascript
+
+// Javascript -> Ecmascript
+// chrome -> V8 -> node
+// firefox -> spidermonkey
+// chrome -> html -> DOM -> blink
+// firefox -> html -> DOM -> gecko
+
+// linguagem funcional
+
+// E -> P -> S
+function soma(n1, n2) 
+{
+    var total = n1 + n2;
+    return total;
+}
+
+soma(1, 2);
+
+// função anonima
+var mult = function (n1, n2)
+{
+    var total = n1 * n2;
+    return total;
+}
+
+// tipos de variaveis
+var nome = "Edir"; //String
+var idade = "14"; //number
+idade = parseInt(idade);
+
+var carro = new String("Corcel");
+var valor = new Number(123);
+
+// O q é um objeto?
+
+// um objeto tem:
+// atributos -> propriedades -> adjetivo -> variavel
+// metodos -> ação -> verbo -> função
+
+
+
+
+
+
